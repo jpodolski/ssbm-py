2968b77291cb63814a9876c64ca6bb1353245f0e
Andrew Orsburn <ao094014@ohio.edu>
2017-11-27 19:37:33 -0500

    improved pylint on Dash and SceneSwitch

--- Dashboard.py
+++ Dashboard.py
@@ -18,44 +18,44 @@ class Dashboard:
         self.main = ttk.Frame(parent)
         self.main.grid()
         self.char_frame_left = ttk.Frame(self.main)
-        self.char_frame_left.grid(column = 0, row = 0)
+        self.char_frame_left.grid(column=0, row=0)
 
         self.score_module_frame = ttk.Frame(self.main)
-        self.score_module_frame.grid(column = 1, row = 0)
+        self.score_module_frame.grid(column=1, row=0)
 
         self.char_frame_right = ttk.Frame(self.main)
-        self.char_frame_right.grid(column = 2, row = 0)
+        self.char_frame_right.grid(column=2, row=0)
 
         self.score_1_frame = ttk.Frame(self.score_module_frame)
         self.score_1 = ScoreCounter(self.score_1_frame)
         self.score_1.set_side(0)
-        self.score_1_frame.grid(row = 1, column = 0)
+        self.score_1_frame.grid(row=1, column=0)
         self.score_frames.append(self.score_1)
 
-        self.hyphen = ttk.Label(self.score_module_frame, text = "-", font = ("Helvetica", 48))
-        self.hyphen.grid(row = 1, column = 1)
+        self.hyphen = ttk.Label(self.score_module_frame, text="-", font=("Helvetica", 48))
+        self.hyphen.grid(row=1, column=1)
 
-        self.update_button = ttk.Button(self.score_module_frame, width = 10, text = "UPDATE", command = lambda: update(self))
-        self.update_button.grid(row = 2, column = 0, columnspan = 4, rowspan = 3, sticky = S)
+        self.update_button = ttk.Button(self.score_module_frame, width=10, text="UPDATE", command=lambda: update(self))
+        self.update_button.grid(row=2, column=0, columnspan=4, rowspan=3, sticky=S)
 
         self.score_2_frame = ttk.Frame(self.score_module_frame)
         self.score_2 = ScoreCounter(self.score_2_frame)
         self.score_2.set_side(1)
-        self.score_2_frame.grid(row = 1, column = 2)
+        self.score_2_frame.grid(row=1, column=2)
         self.score_frames.append(self.score_2)
 
         self.pframe_1 = PlayerFrame(self.char_frame_left)
-        self.pframe_1.pframe.grid(row = 0)
+        self.pframe_1.pframe.grid(row=0)
         self.pframe_1.set_player(1)
         self.pframe_1.align()
 
         self.pframe_2 = PlayerFrame(self.char_frame_right)
-        self.pframe_2.pframe.grid(row = 0)
+        self.pframe_2.pframe.grid(row=0)
         self.pframe_2.set_player(2)
         self.pframe_2.align()
 
         self.pframe_3 = PlayerFrame(self.char_frame_left)
-        self.pframe_3.pframe.grid(row = 1)
+        self.pframe_3.pframe.grid(row=1)
         self.pframe_3.set_player(3)
         self.pframe_3.align()
 
--- SceneSwitch.py
+++ SceneSwitch.py
@@ -13,9 +13,9 @@ class SceneSwitch:
     def __init__(self, parent):
         self.main = ttk.Frame(parent)
         self.main.grid()
-        self.scene_a = ttk.Button(self.main, text = "Scene A", command = lambda: write_scene("SCENE_A"))
-        self.scene_b = ttk.Button(self.main, text = "Scene B", command = lambda: write_scene("SCENE_B"))
-        self.scene_c = ttk.Button(self.main, text = "Scene C", command = lambda: write_scene("SCENE_C"))
+        self.scene_a = ttk.Button(self.main, text="Scene A", command=lambda: write_scene("SCENE_A"))
+        self.scene_b = ttk.Button(self.main, text="Scene B", command=lambda: write_scene("SCENE_B"))
+        self.scene_c = ttk.Button(self.main, text="Scene C", command=lambda: write_scene("SCENE_C"))
         self.scene_a.pack()
         self.scene_b.pack()
         self.scene_c.pack()
