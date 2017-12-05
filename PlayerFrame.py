660193b33469b313321e6b23faed4bece5b94d4a
Jeff Podolski <jpodolski@protonmail.com>
2017-12-05 14:07:59 -0500

    more pylint changes

--- PlayerFrame.py
+++ PlayerFrame.py
@@ -18,105 +18,100 @@ Github Repo: http://github.com/jpodolski/ssbm-py
 Sharing License: https://creativecommons.org/licenses/by/4.0/
 """
 # ==============================================================================
-
 from tkinter import *
 import tkinter.ttk as ttk
-from file_manip import *
+from file_manip import gen_char_filename
 from match import Match
 from PIL import Image, ImageTk
 from information import *
-import math
+# import math
 
 
-class PlayerFrame:
+class PlayerFrame(object):
     """
     A GUI class that contains the components responsible
     for editing one Player item in the InfoTab
     """
 
-    """ INITIALIZATION """
-        
     def __init__(self, parent):
         """ Initialize the GUI elements and private variables. """
         self.__player = 0
         self.__port = 0
         self.__tkport = StringVar()
-        self.__tkport.set("Player" + str(self.__player))
+        self.__tkport.set("Player"+str(self.__player))
         self.__tkchar = StringVar()
         self.__tkchar.set("Generic")
         self.__char = 26
         self.__sub_color = 0
         self.__image = Image.open("media/stock/26_0.png")
         self.__tkimg = ImageTk.PhotoImage(self.__image)
-        self.pframe = ttk.LabelFrame(parent, text = "Player" + str(self.__player), relief="sunken", borderwidth=10)
-        self.pframe.grid(row = 0, column = self.__player)
+        self.pframe = ttk.LabelFrame(parent, relief="sunken", borderwidth=10)
+        self.pframe.configure(text="Player"+str(self.__player))
+        self.pframe.grid(row=0, column=self.__player)
         self.tag_label = ttk.Label(self.pframe, text="Tag:")
-        self.tag_label.grid(row = 0, column = 0, sticky = W)
+        self.tag_label.grid(row=0, column=0, sticky='W')
 
         self.port_label = ttk.Label(self.pframe, text="Port:")
-        self.port_label.grid(row = 1, column = 5, sticky = W)
+        self.port_label.grid(row=1, column=5, sticky='W')
 
         self.prefix_label = ttk.Label(self.pframe, text="Prefix:")
-        self.prefix_label.grid(row = 0, column = 2, columnspan=2, sticky = W)
+        self.prefix_label.grid(row=0, column=2, columnspan=2, sticky='W')
 
         self.tag = ttk.Entry(self.pframe)
-        self.tag.grid(row = 0, column = 1, columnspan = 1)
+        self.tag.grid(row=0, column=1, columnspan=1)
 
         self.prefix = ttk.Entry(self.pframe)
-        self.prefix.grid(row = 0, column = 4, columnspan = 3)
+        self.prefix.grid(row=0, column=4, columnspan=3)
 
-        self.port_dropdown = ttk.OptionMenu(self.pframe, self.__tkport, command = lambda _: self.select_port(), *ports)
-        self.port_dropdown.grid(row = 1, column = 6, columnspan = 1, sticky = EW)
+        self.port_dropdown = ttk.OptionMenu(self.pframe, self.__tkport, command=lambda _:self.select_port(), *ports)
+        self.port_dropdown.grid(row=1, column=6, columnspan=1, sticky='EW')
 
-        self.char_dropdown = ttk.OptionMenu(self.pframe, self.__tkchar, command = lambda _: self.select_character(self), *character_names)
-        self.char_dropdown.grid(row = 1, column = 0, columnspan = 2, sticky = EW)
+        self.char_dropdown = ttk.OptionMenu(self.pframe, self.__tkchar, command=lambda _: self.select_character(self), *character_names)
+        self.char_dropdown.grid(row=1, column=0, columnspan=2, sticky='EW')
 
-        self.char_prevcolor = ttk.Button(self.pframe, text = "<", width = 1)
-        self.char_prevcolor.config(command = lambda: self.inc_color(-1))
-        self.char_prevcolor.grid(row = 1, column = 2)
+        self.char_prevcolor = ttk.Button(self.pframe, text="<", width=1)
+        self.char_prevcolor.config(command=lambda: self.inc_color(-1))
+        self.char_prevcolor.grid(row=1, column=2)
 
-        self.char_icon = ttk.Label(self.pframe, image = self.__tkimg)
-        self.char_icon.grid(row = 1, column = 3)
+        self.char_icon = ttk.Label(self.pframe, image=self.__tkimg)
+        self.char_icon.grid(row=1, column=3)
 
-        self.char_nextcolor = ttk.Button(self.pframe, text = ">", width = 1)
-        self.char_nextcolor.config(command = lambda: self.inc_color(1))
-        self.char_nextcolor.grid(row = 1, column = 4)
-
-    """ ACCESSORS """
+        self.char_nextcolor = ttk.Button(self.pframe, text=">", width=1)
+        self.char_nextcolor.config(command=lambda: self.inc_color(1))
+        self.char_nextcolor.grid(row=1, column=4)
 
     def get_tag(self):
         """ Returns tag as string """
-        return(self.tag.get())
+        return self.tag.get()
 
     def get_char(self):
         """ Returns char as int """
-        return(self.__char)
+        return self.__char
 
     def get_sub_color(self):
         """ Returns sub-color as int (0-N) """
-        return(self.__sub_color)
+        return self.__sub_color
 
     def get_port(self):
         """ Returns port as int (1-4) """
-        return("PLACEHOLDER")
+        return "PLACEHOLDER"
 
     def get_prefix(self):
         """ Returns prefix as string """
-        return("PLACEHOLDER")
+        return "PLACEHOLDER"
 
     def set_player(self, new_player):
+        """ sets __player """
         self.__player = new_player
-
-    """ INTERNAL FUNCTIONS """
 
     def align(self):
         """ Arranges modules inside the frame and sets the appropriate Label text """
         # self.pframe.grid(row = math.floor((self.__player-1)/2), column = (self.__player-1)%2)
-        self.pframe.config(text = "Player" + str(self.__player))
+        self.pframe.config(text="Player"+str(self.__player))
 
     def inc_color(self, amt):
         """ Adds the value amt to __sub_color and refreshes image. Called by char_nextcolor and char_prevcolor """
-        self.__sub_color = (self.__sub_color + amt)%(char_mod_table[self.__char]+1)
+        self.__sub_color = (self.__sub_color+amt)%(char_mod_table[self.__char]+1)
         self.__image = Image.open(gen_char_filename(self.__char, self.__sub_color))
         self.__tkimg = ImageTk.PhotoImage(self.__image)
         self.char_icon.configure(image = self.__tkimg)
@@ -127,7 +122,7 @@ class PlayerFrame:
         self.__sub_color = 0
         self.__image = Image.open(gen_char_filename(self.__char, self.__sub_color))
         self.__tkimg = ImageTk.PhotoImage(self.__image)
-        self.char_icon.configure(image = self.__tkimg)
+        self.char_icon.configure(image=self.__tkimg)
 
 
 
--- SceneSwitch.py
+++ SceneSwitch.py
@@ -1,14 +1,31 @@
-from tkinter import *
-from PlayerFrame import *
-from ScoreCounter import *
-from file_manip import *
+#!/usr/bin/python3
 
-class SceneSwitch:
+## @file SceneSwitch.py
+## @author Jeff Podolski
+## @todo
+#   - Basically everything...
+#   - Json parser and auto-setup, etc
+
+# ==============================================================================
+"""
+SCENESWITCH.PY
+
+Attribution 4.0 International (CC BY 4.0)
+All code below is free and open source, intended to better the smash community
+You are free to copy, modify, and redistribute this code as long as credit
+is given to the original author (Jeff Podolski). Check out the source!
+Github Repo: http://github.com/jpodolski/ssbm-py
+Sharing License: https://creativecommons.org/licenses/by/4.0/
+"""
+# ==============================================================================
+
+import tkinter.ttk as ttk
+from file_manip import write_scene
+
+class SceneSwitch(object):
     """
     A GUI class for the Scene Switcher tab
     """
-
-    """ INITIALIZATION """
 
     def __init__(self, parent):
         self.main = ttk.Frame(parent)
