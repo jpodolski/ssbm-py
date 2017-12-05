2b5a2d16d7e74be5a0c1b905b72f7fc1bd590f3d
Jeff Podolski <jpodolski@protonmail.com>
2017-12-05 10:36:03 -0500

    emergency fix for var names in gui.py

--- information.py
+++ information.py
@@ -23,42 +23,35 @@ Sharing License: https://creativecommons.org/licenses/by/4.0/
 # ==============================================================================
 
 # All characters that can be played with in SSBM
-character_names = [
-	"Fox", 
-	"Falco",  
-	"Marth", 
-	"Sheik", 
-	"Jigglypuff", 
-	"Peach", 
-	"Captain Falcon", 
-	"Ice Climbers", 
-	"Samus", 
-	"Pikachu", 
-	"Luigi", 
-	"Yoshi", 
-	"Doctor Mario", 
-	"Ganondorf", 
-	"Mario", 
-	"Young Link", 
-	"Link", 
-	"Donkey Kong", 
-	"Mr. Game & Watch",
-	"Roy", 
-	"Mewtwo", 
-	"Zelda", 
-	"Pichu", 
-	"Ness", 
-	"Bowser", 
-	"Kirby", 
-	"Generic" 
-]
+character_names = []
+character_names.append("Fox")
+character_names.append("Falco")
+character_names.append("Marth")
+character_names.append("Sheik")
+character_names.append("Jigglypuff")
+character_names.append("Peach")
+character_names.append("Captain Falcon")
+character_names.append("Ice Climbers")
+character_names.append("Samus")
+character_names.append("Pikachu")
+character_names.append("Luigi")
+character_names.append("Yoshi")
+character_names.append("Doctor Mario")
+character_names.append("Ganondorf")
+character_names.append("Mario")
+character_names.append("Young Link")
+character_names.append("Link")
+character_names.append("Donkey Kong")
+character_names.append("Mr. Game & Watch")
+character_names.append("Roy")
+character_names.append("Mewtwo")
+character_names.append("Zelda")
+character_names.append("Pichu")
+character_names.append("Ness")
+character_names.append("Bowser")
+character_names.append("Kirby")
+character_names.append("Generic")
 
+ports = ["Player 1", "Player 2", "Player 3", "Player 4"]
 
-ports = [
-	"Player 1", 
-	"Player 2", 
-	"Player 3", 
-	"Player 4" 
-]
-
-char_mod_table = [3, 3, 4, 4, 4, 4, 5, 3, 4, 3, 3, 5, 4, 4, 4, 4, 4, 4, 3, 4, 3, 4, 3, 3, 3, 5, 0] 
+char_mod_table = [3, 3, 4, 4, 4, 4, 5, 3, 4, 3, 3, 5, 4, 4, 4, 4, 4, 4, 3, 4, 3, 4, 3, 3, 3, 5, 0]
